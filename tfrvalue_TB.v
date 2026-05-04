198da991187f69c4dae1db03df7d041443dd1400
vetal-tue <bacchic@gmail.com>
2026-05-04 18:28:20 +0300

    Update tfrvalue_TB.v

--- tfrvalue_TB.v
+++ tfrvalue_TB.v
@@ -33,29 +33,47 @@ initial begin
 end
 
 
-// initial begin
-// 	reset = 1'b1;
-// // #400000 @(negedge pll_not_locked_out)
-// // 		reset = 1'b0;
-// #2000000
-// 		reset = 1'b0;
-// end
+initial begin
+  $dumpfile("tfrvalue_TB.vcd");
+  $dumpvars(0,tfrvalue_TB);
+end
 
-wire [31:0] tdata;
-wire        tvalid;
-wire        tready;
+integer cycle_count = 0;
+
+// 3. Блок для подсчета тактов
+always @(posedge clk1) begin
+    cycle_count = cycle_count + 1;
+end
+
+// 4. Основной блок тестирования
+initial begin
+    $display("Simulation start.");
+
+    // Ждем N тактов. Например, 100.
+    wait (cycle_count == 200);
+
+    $display("Simulation stopped after %0d cycle_counts.", cycle_count);
+    $finish; // Останавливаем симуляцию
+end
+
+wire [31:0] tdata_in;
+wire        tvalid_in;
+wire        tready_in;
+
+wire [31:0] tdata_out;
+wire        tvalid_out;
 
 Simple_AXIS_master    Simple_AXIS_master    (
         // AXI TX Interface
   .clk                (clk1),
   .reset              (reset),
-  .m_axi_tdata        (tdata),
-  .m_axi_tvalid       (tvalid),
+  .m_axi_tdata        (tdata_in),
+  .m_axi_tvalid       (tvalid_in),
   .m_axi_tlast        (),
   .m_axi_tkeep        (),
   .channel_up         (1'b1),
   .lane_up            (1'b1),
-  .m_axi_tready       (tready),
+  .m_axi_tready       (tready_in),
   .s_axi_tdata        (32'b0),
   .s_axi_tvalid       (1'b0),
   .s_axi_tlast        (1'b0),
@@ -74,14 +92,14 @@ tfrvalue              tfrvalue    (
         // AXI TX Interface
   .i_a_clk            (clk1),
   .i_a_reset          (reset),
-  .i_a_valid          (tvalid),
-  .o_a_ready          (tready),
-  .i_a_data           (tdata),
+  .i_a_valid          (tvalid_in),
+  .o_a_ready          (tready_in),
+  .i_a_data           (tdata_in),
   .i_b_clk            (clk2),
-  .i_b_reset          (),
-  .o_b_valid          (),
+  .i_b_reset          (reset),
+  .o_b_valid          (tvalid_out),
   .i_b_ready          (1'b1),
-  .o_b_data           ()
+  .o_b_data           (tdata_out)
 
   );
 
